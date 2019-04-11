772baafc96774d3c96b69b6625d05f68a509c488
Suyog <suyog.sawant@coverfoxmail.com>
2019-04-12 00:28:52 +0530

    basic project setup

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render (
+<App color="Blue" />,
+  document.getElementById("root")
+);
